a774c263402304b33bf3374c53d4d1634d98cb75
Christian Parpart <christian@parpart.family>
2020-09-02 21:05:44 +0200

    thoughts

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,20 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 #include <array>
+#include <atomic>
 #include <cmath>
+#include <csignal>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
 #include <tuple>
+#include <vector>
+
+#include <portaudio.h>
+
+// https://codereview.stackexchange.com/questions/148139/pitch-detection-library-basic-architecture?newreg=35d0275e30a94b1a973a6058ba1b88ac
 
 using namespace std;
 
@@ -32,7 +40,7 @@ inline tuple<Octave, Semitone, Percentile> freq2semitone(float freq, float conce
 {
     // TODO: this isn't quite working yet.
     float const semitoneF = octaveNames.size() * log2f(freq / concertA);
-    Percentile const percentile = (static_cast<int>(semitoneF * 100.0f) % 100) / 100.f;
+    Percentile const percentile = static_cast<float>(static_cast<int>(semitoneF * 100.0f) % 100) / 100.f;
     Octave const octave = log2f(freq / concertA) + 4;
     Semitone semitone = static_cast<Semitone>(semitoneF);
 
@@ -74,7 +82,7 @@ void printNoteFromFreqency(float freq, float concertA)
 /// @param concertA       the concert-A (A4) to base all other musical notes on.
 void printMusicalNote(unsigned octave, unsigned semitoneOffset, float concertA)
 {
-    float const freq = concertA * pow(2.0f, -4 + static_cast<int>(octave) + semitoneOffset / static_cast<float>(octaveNames.size()));
+    float const freq = concertA * pow(2.0f, -4.0f + static_cast<float>(octave) + static_cast<float>(semitoneOffset) / static_cast<float>(octaveNames.size()));
     printf("%d: %-2s %8f (relative semitone: %d)\n",
            octave,
            octaveNames[semitoneOffset % octaveNames.size()],
@@ -83,12 +91,12 @@ void printMusicalNote(unsigned octave, unsigned semitoneOffset, float concertA)
     );
 }
 
-int main(int argc, char* argv[])
+int old_main(int argc, char* argv[]) // {{{
 {
     if (argc >= 3 && strcmp(argv[1], "-f") == 0)
     {
-        float const freq = atof(argv[2]);
-        float const concertA = argc == 4 ? atof(argv[3]) : 440.0f;
+        float const freq = static_cast<float>(atof(argv[2]));
+        float const concertA = argc == 4 ? static_cast<float>(atof(argv[3])) : 440.0f;
 
         printNoteFromFreqency(freq, concertA);
     }
@@ -96,7 +104,7 @@ int main(int argc, char* argv[])
     {
         int const octaveBeg = argc >= 2 ? atoi(argv[1]) : 2;
         int const octaveEnd = argc >= 3 ? atoi(argv[2]) : octaveBeg;
-        float const referenceA4 = argc == 4 ? atof(argv[3]) : 440.0f;
+        float const referenceA4 = argc == 4 ? static_cast<float>(atof(argv[3])) : 440.0f;
 
         for (int octave = octaveBeg; octave <= octaveEnd; ++octave)
             for (unsigned i = 0; i < octaveNames.size(); ++i)
@@ -106,5 +114,140 @@ int main(int argc, char* argv[])
     }
 
     return EXIT_SUCCESS;
+} // }}}
+
+class PitchDetector
+{
+  private:
+    using Sample = float;
+
+    int channelCount_ = 1;
+    PaStream* stream_ = nullptr;
+
+    std::atomic<unsigned> count_ = 0;
+    long frameIndex_ = 0;
+    std::vector<Sample> recordedSamples_;
+
+  public:
+    std::atomic<bool> terminating = false;
+
+    PitchDetector() : PitchDetector(44100, 512, 1) {}
+
+    PitchDetector(int _sampleRate, int _framesPerBuffer, int _numChannels) :
+        channelCount_(_numChannels)
+    {
+        instance = this;
+
+        Pa_Initialize();
+
+        auto const numSeconds = 2;
+        auto const numFrames = numSeconds * _sampleRate;
+        auto const numSamples = numFrames * _numChannels;
+        recordedSamples_.resize(numSamples, Sample{});
+
+        PaStreamParameters inputParameters;
+        inputParameters.device = Pa_GetDefaultInputDevice();
+        inputParameters.channelCount = channelCount_;
+        inputParameters.sampleFormat = paFloat32;
+        inputParameters.suggestedLatency = Pa_GetDeviceInfo( inputParameters.device )->defaultLowInputLatency;
+        inputParameters.hostApiSpecificStreamInfo = nullptr;
+
+        PaStreamCallback sc;
+        Pa_OpenStream(&stream_,
+                      &inputParameters,
+                      nullptr, // outputParameters
+                      _sampleRate,
+                      _framesPerBuffer,
+                      paClipOff,
+                      &PitchDetector::streamCallback,
+                      this);
+    }
+
+    ~PitchDetector()
+    {
+        Pa_Terminate();
+        instance = nullptr;
+    }
+
+    static int streamCallback(void const* input,
+                               void* output,
+                               unsigned long frameCount,
+                               PaStreamCallbackTimeInfo const* timeInfo,
+                               PaStreamCallbackFlags statusFlags,
+                               void* userdata)
+    {
+        PitchDetector* pd = (PitchDetector*) userdata;
+        return pd->input(input, output, frameCount, timeInfo, statusFlags);
+    }
+
+    int input(void const* inputBuffer,
+              void* outputBuffer,
+              unsigned long frameCount,
+              PaStreamCallbackTimeInfo const* timeInfo,
+              PaStreamCallbackFlags statusFlags)
+    {
+        (void) outputBuffer;
+        (void) timeInfo;
+        (void) statusFlags;
+
+        count_ += frameCount;
+
+        Sample const* rptr = (Sample const*) inputBuffer;
+
+        for (unsigned long i = 0; i < frameCount; ++i)
+        {
+            Sample* wptr = &recordedSamples_[(frameIndex_ * channelCount_ + i) % recordedSamples_.size()];
+            *wptr = *rptr;
+            ++wptr;
+            ++rptr;
+
+            if (channelCount_ == 2)
+                *wptr++ = *rptr++;
+        }
+        frameIndex_ += static_cast<long>(frameCount);
+
+        return terminating.load() ? paComplete : paContinue;
+    }
+
+    void run()
+    {
+        Pa_StartStream(stream_);
+        PaError err = paNoError;
+        while ((err = Pa_IsStreamActive(stream_)) == 1)
+        {
+            Pa_Sleep(1000); // 1sec
+
+            auto count = count_.load();
+            while (!count_.compare_exchange_strong(count, 0))
+                count = count_.load();
+
+            printf("count: %u\n", count);
+            fflush(stdout);
+        }
+        Pa_CloseStream(stream_);
+    }
+
+    static PitchDetector* instance;
+};
+
+PitchDetector* PitchDetector::instance = nullptr;
+
+void signalHandler(int _signo)
+{
+    signal(_signo, SIG_DFL);
+    printf("\nsignal caught: %d (%s)\n", _signo, strsignal(_signo));
+    PitchDetector::instance->terminating = true;
 }
-// vim:et
+
+int main(int argc, char* argv[])
+{
+    (void) argc;
+    (void) argv;
+
+    signal(SIGINT, &signalHandler);
+
+    PitchDetector pd;
+    pd.run();
+
+    return 0;
+}
